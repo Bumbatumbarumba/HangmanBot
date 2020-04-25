ceb6e9e40169f5f023388c54bf7e95396f96d8cd
Bartosz <bartosz_@live.ca>
2020-04-24 20:00:15 -0400

    made the program gracefully respond to errors

--- src/index.js
+++ src/index.js
@@ -22,54 +22,58 @@ client.on("ready", () => {
 })
 
 client.on("message", message => {
-    if (message.content.startsWith("!hb")) {
-        secondpart = message.content.split(" ")[1]
-
-        if (secondpart == "start") {
-            if (!gameStarted) {
-                gameStarted = true
-                botfuncs.InitRound()
-                message.reply(botfuncs.DisplayMan())
-            }
-            else {
-                message.reply("Game in progress!")
-            }
-        }
-        else if (secondpart == "stop") {
-            if (gameStarted) {
-                gameStarted = false
-                message.reply("Stopping game, please enter '!hb start' to start a new round.")
-            }
-            else {
-                message.reply("No game to stop!")
-            }
-        }
-        else if (secondpart == "help") {
-            message.reply("List of commands:\n" + "!hb start - starts the game,\n" + "!hb stop - stops the game,\n" + "!hb help - shows the list of commands,\n" + "!hb scores - displays scores if there are any,\n" + "!hb \"guess\" - put your guess in quotation marks")
-        } 
-        else if (secondpart == "scores") {
-            message.reply("These are the scores: " + JSON.stringify(currentScores))
-        } 
-        else if (secondpart[0] == "\"") {
-            if (gameStarted){
-                latestSender = message.member.user.tag
-                res = botfuncs.ParseGuess(secondpart)
-                if (res.indexOf("found the word,") !== -1) {
-                    updateScores(latestSender)
-                    gameStarted = false
+    try {
+        if (message.content.startsWith("!hb")) {
+            secondpart = message.content.split(" ")[1]
+    
+            if (secondpart == "start") {
+                if (!gameStarted) {
+                    gameStarted = true
+                    botfuncs.InitRound()
+                    message.reply(botfuncs.DisplayMan())
                 }
-                else if (res.indexOf("game over!") !== -1) {
-                    gameStarted = false
+                else {
+                    message.reply("Game in progress!")
                 }
-                message.reply(res)
             }
+            else if (secondpart == "stop") {
+                if (gameStarted) {
+                    gameStarted = false
+                    message.reply("Stopping game, please enter '!hb start' to start a new round.")
+                }
+                else {
+                    message.reply("No game to stop!")
+                }
+            }
+            else if (secondpart == "help") {
+                message.reply("List of commands:\n" + "!hb start - starts the game,\n" + "!hb stop - stops the game,\n" + "!hb help - shows the list of commands,\n" + "!hb scores - displays scores if there are any,\n" + "!hb \"guess\" - put your guess in quotation marks")
+            } 
+            else if (secondpart == "scores") {
+                message.reply("These are the scores: " + JSON.stringify(currentScores))
+            } 
+            else if (secondpart[0] == "\"") {
+                if (gameStarted){
+                    latestSender = message.member.user.tag
+                    res = botfuncs.ParseGuess(secondpart)
+                    if (res.indexOf("found the word,") !== -1) {
+                        updateScores(latestSender)
+                        gameStarted = false
+                    }
+                    else if (res.indexOf("game over!") !== -1) {
+                        gameStarted = false
+                    }
+                    message.reply(res)
+                }
+                else {
+                    message.reply("Please type '!hb start' to start a game.")
+                }
+            } 
             else {
-                message.reply("Please type '!hb start' to start a game.")
+                message.reply("Unrecognized command! Type '!hb help' to see what I can do!")
             }
-        } 
-        else {
-            console.log("Unrecognized command! Type '!hb help' to see what I can do!")
         }
+    } catch (error) {
+        message.reply("Unrecognized command! Type '!hb help' to see what I can do!")
     }
 })
 
